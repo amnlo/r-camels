c4c68c34da637d361dfee961d4a225a3368a3f1d
amnlo <loam.eawag@gmail.com>
2020-02-08 20:47:02 +0100

    Preproc. of catch. property data

--- functions/preprocess.data.r
+++ functions/preprocess.data.r
@@ -1,32 +1,72 @@
-preprocess.data <- function(all.data, lags=60, n.catchments=10){
+preprocess.data <- function(all.data, lags=60, n.catchments=10, prop.separate=TRUE){
   set.seed(9)
-  catch.ids <- all.data$attrib$gauge_id[sample(1:nrow(all.data$attrib), size=n.catchments, replace=F)]
+  catch.ids <- all.data$attrib$gauge_id[1:n.catchments]
   all.data.small <- list(strmflw = all.data$strmflw[which(as.numeric(names(all.data$strmflw)) %in% catch.ids)],
-                         forcing = all.data$forcing[which(as.numeric(names(all.data$forcing)) %in% catch.ids)])
-  ## preprocess forcing data
+                         forcing = all.data$forcing[which(as.numeric(names(all.data$forcing)) %in% catch.ids)],
+                         attrib  = all.data$attrib %>% filter(gauge_id %in% catch.ids))
+  
+  ## preprocess catchment property data
+  notneed <- c("huc_02", "gauge_name", "gauge_lat", "gauge_lon", "area_geospa_fabric") # unwanted column names
+  need <- c("gauge_id","p_mean","pet_mean","p_seasonality","frac_snow","aridity","area_gages2")
+  prop <- all.data.small$attrib %>% select(need)
+  if(n.catchments>1){
+    prop <- prop %>% mutate(area_scld=(area_gages2-min(area_gages2))/(diff(range(area_gages2))))
+  }else{
+    prop <- prop %>% mutate(area_scld=0)
+  }
+  
+  
+  
+  ## preprocess forcing data (and include catchment property data)
   forc <- rbindlist(all.data.small$forcing, idcol = "gauge_id") # spread the list into a data frame
+  forc <- forc %>% filter(Year>1990 & Year < 2000)
   forc <- forc %>% select(-dayl.s., -swe.mm.)
-  forc <- forc %>% mutate(gauge_id = as.numeric(gauge_id))
+  # Scale forcing data
   forc <- forc %>% mutate(prcp.mm.day. = ((prcp.mm.day.+ 0.1)^0.2 - 1) / 0.2) # Box-Cox transformation
   forc <- forc %>% mutate(avg.temp = (tmax.C. + tmin.C.) /2) %>% mutate(avg.temp = (avg.temp-min(avg.temp))/(max(avg.temp)-min(avg.temp)))
   forc <- forc %>% mutate(srad.W.m2. = scale_this(srad.W.m2.)$x)
   forc <- forc %>% mutate(vp.Pa. = scale_this(vp.Pa.)$x)
-  forc <- forc %>% select(-tmin.C., -tmax.C.)
-  tmp <- lapply(X = 1:(nrow(forc)-lags), FUN = function(x,data) data[x:(x+lags-1),], data=forc)
-  tmp <- array(unlist(tmp), dim=c(lags,ncol(forc),length(tmp)))
-  dimnames(tmp)[[2]] <- colnames(forc)
-  dat <- aperm(tmp, c(3,1,2))
+  forc <- forc %>% mutate(gauge_id = as.numeric(gauge_id))
+  forc <- forc %>% select(-tmin.C., -tmax.C.) %>% arrange(gauge_id, Year, Mnth, Day)
+  forc.l <- list()
+  for(c.curr in unique(forc$gauge_id)){
+    ind <- which(forc$gauge_id == c.curr)
+    if(prop.separate){
+      forc.prop <- forc[ind,]      
+    }else{
+      ## add property data (values are repeated for each time step)
+      forc.prop <- cbind(forc[ind,], prop %>% filter(gauge_id==c.curr) %>% select(-area_gages2)) ## remove unscaled area (scaled is still in there)
+    }
+    tmp <- lapply(X = 1:(length(ind)-lags), FUN = function(x,data) data[x:(x+lags),], data = forc.prop)
+    tmp <- array(unlist(tmp), dim=c(lags+1,ncol(forc.prop),length(tmp)))
+    dimnames(tmp)[[2]] <- colnames(forc.prop)
+    dat <- aperm(tmp, c(3,1,2))
+    forc.l[[as.character(c.curr)]] <- dat
+  }
+  forc.new <- do.call(abind, list(forc.l, along=1))
+  forc.new <- forc.new[,,!(dimnames(forc.new)[[3]] %in% c("gauge_id","Year","Mnth","Day","Hr"))]
   
   ## preprocess streamflow data
   strm <- rbindlist(all.data.small$strmflw, idcol="gauge_id")
   strm <- strm %>% mutate(gauge_id = as.numeric(gauge_id))
   strm <- strm %>% select(-V6)
   colnames(strm)[2:5] <- c("Year","Mnth","Day","Q")
+  strm <- strm  %>% filter(Year > 1990 & Year < 2000)
+  if(any(strm[,"Q"]==-999)) stop("negative streamflow (= NA) encountered")
+  if(nrow(strm) != nrow(forc)) warning("streamflow dimension does not agree with forcing data")
+  ## make sure streamflow rows correspond to forcing rows
+  joint <- merge(forc, strm, by=c("gauge_id","Year","Mnth","Day")) %>% arrange(gauge_id, Year, Mnth, Day)
+  joint <- merge(joint, prop %>% select(gauge_id, area_gages2), by="gauge_id") %>% mutate(Q=Q/area_gages2) %>% select(-area_gages2)
+  strm <- joint %>% select(gauge_id, Q)
+  for(c.curr in unique(strm$gauge_id)){
+    ind <- which(strm$gauge_id==c.curr)
+    strm <- strm %>% slice(-ind[1:lags])
+  }
+  strm <- strm %>% select(Q)
+  ## scale streamflow data
+  tmp <- scale.log(strm[,1])
+  strm[,1] <- tmp$y
   
-  ## preprocess catchment property data
-  notneed <- c("huc_02", "gauge_name", "gauge_lat", "gauge_lon", "area_geospa_fabric") # unwanted column names
-  prop <- all.data.small$attrib %>% select(-notneed)
-  
-  
+  data <- list(x=forc.new, y=strm, scl=tmp$scl)
   return(data)
 }
--- run/run.r
+++ run/run.r
@@ -5,19 +5,13 @@ library(tensorflow)
 library(keras)
 library(ggplot2)
 library(data.table)
-
-
+library(abind)
 
 source("../functions/read.catchment.attributes.r")
 source("../functions/read.forcing.r")
 source("../functions/read.strmflw.r")
-
-scale_this <- function(x){
-  mn <- mean(x, na.rm=TRUE)
-  sd <- sd(x, na.rm=TRUE)
-  x.new <- (x - mn) / sd
-  return(list(x=x.new, center=mn, scale=sd))
-}
+source("../functions/preprocess.data.r")
+source("../functions/scale.log.r")
 
 # pth <- "../camels_attributes_v2_0/camels_attributes_v2_0/data/"
 # attrib <- read.catchment.attributes(pth)
@@ -29,84 +23,37 @@ scale_this <- function(x){
 # save(all.data, file="../all_data.RData")
 load("../all_data.RData")
 
-## reshape forcing data into nice format
-dims <- list()
-n.row <- 0
-n.col <- 0
-for(forc in names(all.data$forcing)[1:2]){
-  dims[[forc]] <- dim(all.data$forcing[[forc]])
-  n.row <- n.row + dims[[forc]][1]
-  n.col <- max(n.col, dims[[forc]][2])
-}
-forc.dat <- matrix(0.0, nrow=n.row, ncol=n.col)
-i <- 1
-for(curr in dims[1:2]){
-  if(i==1){
-    print(curr)
-    forc.dat[1:(curr[1]),1:n.col] <- as.numeric(as.matrix(all.data$forcing[[i]]))
-    row.curr <- curr[1]+1
-  }
-  else{
-    print(curr)
-    forc.dat[row.curr:(row.curr+curr[1]-1),1:n.col] <- as.numeric(as.matrix(all.data$forcing[[i]]))
-    row.curr <- row.curr + curr[1]
-  }
-  i <- i+1
-}
-colnames(forc.dat) <- colnames(all.data$forcing[[1]])
-
-## put catchment properties data into nice format
-prop <- all.data$attrib
-notneed <- c("huc_02", "gauge_name", "gauge_lat", "gauge_lon", "area_geospa_fabric") # unwanted column names
-prop <- prop %>% select(-notneed)
-
-## prepare data for 1 catchment (as example of data format needed)
-id <- "01013500"
-forc1.orig <- all.data$forcing[[id]] %>% select(-Year, -Mnth, -Day, -Hr, -dayl.s., -swe.mm.)
-forc1 <- forc1.orig
-forc1 <- forc1 %>% mutate(prcp.mm.day. = ((prcp.mm.day.+ 0.1)^0.2 - 1) / 0.2) # Box-Cox transformation
-forc1 <- forc1 %>% mutate(avg.temp = (tmax.C. + tmin.C.) /2) %>% mutate(avg.temp = (avg.temp-min(avg.temp))/(max(avg.temp)-min(avg.temp)))
-forc1 <- forc1 %>% mutate(srad.W.m2. = scale_this(srad.W.m2.)$x)
-forc1 <- forc1 %>% mutate(vp.Pa. = scale_this(vp.Pa.)$x)
-forc1 <- forc1 %>% select(-tmin.C., -tmax.C.)
 lags <- 60
-tmp <- lapply(X = 1:(nrow(forc1)-lags), FUN = function(x,data) data[x:(x+lags-1),], data=forc1)
-tmp <- array(unlist(tmp), dim=c(lags,ncol(forc1),length(tmp)))
-dimnames(tmp)[[2]] <- colnames(forc1)
-dat <- aperm(tmp, c(3,1,2))
-y <- all.data$strmflw[[id]] %>% select(V5) %>% slice((lags+1):nrow(.))
-y <- as.numeric(y$V5)
-rm <- which(y != -999)
-y <- y[rm]
-scld <- scale_this(y)
-y <- scld$x
-dat <- dat[rm,,]
+data <- preprocess.data(all.data, lags=lags, n.catchments = 200, prop.separate = FALSE)
+x <- data$x
+y <- data$y[,1]
 
-batch.size <- 20
+## ==================================================================
+## split into training and test data
+batch.size <- 32
 validation_split <- 0.2
-even.train <- ((nrow(dat)*(1-validation_split))%/%batch.size)*batch.size
-even.test  <- ((nrow(dat)*validation_split)%/%batch.size)*batch.size
-x.train <- dat[1:even.train,,]
-x.test <-  dat[(even.train+1):(even.train+even.test),,]
+even.train <- ((nrow(x)*(1-validation_split))%/%batch.size)*batch.size
+even.test  <- ((nrow(x)*validation_split)%/%batch.size)*batch.size
+x.train <- x[1:even.train,,]
+x.test <-  x[(even.train+1):(even.train+even.test),,]
 y.train <- y[1:even.train]
 y.test  <- y[(even.train+1):(even.train+even.test)]
 
-## construct LSTM for one catchment
+## ==================================================================
+## construct LSTM
 model <- keras_model_sequential()
-model %>% layer_lstm(units = lags,
-             input_shape = c(lags, ncol(forc1)),
-             batch_size = batch.size,
-             return_sequences = TRUE,
-             stateful = TRUE) %>%
-  layer_dropout(rate = 0.5) %>%
-  layer_lstm(units = 50,
-             return_sequences = FALSE,
-             stateful = TRUE) %>%
-  layer_dropout(rate = 0.5) %>%
+model %>% layer_lstm(units = 64,
+                     input_shape = c(lags+1, dim(x.train)[3]),
+                     batch_size = batch.size,
+                     stateful = FALSE) %>%
+  layer_dropout(rate = 0.4) %>%
   layer_dense(units = 1)
-model %>% compile(loss = 'mse', optimizer = 'adam', metrics='accuracy')
+model %>% compile(loss = 'mse', optimizer = 'adam', metrics='mse')
 
+## ==================================================================
+## make sure to save checkpoints while training
 checkpoint_dir <- "checkpoints"
+dir.create(checkpoint_dir)
 filepath <- file.path(checkpoint_dir, "weights.{epoch:02d}-{val_loss:.2f}.hdf5")
 # Create checkpoint callback
 cp_callback <- callback_model_checkpoint(
@@ -114,29 +61,42 @@ cp_callback <- callback_model_checkpoint(
   save_weights_only = TRUE,
   verbose = 1
 )
+## ==================================================================
+## train model
 model %>% fit(x = x.train,
                          y = y.train,
                          batch_size = batch.size,
-                         epochs = 5,
+                         epochs = 10,
                          callbacks = list(cp_callback),
                          view_metrics = TRUE,
                          validation_data = list(x.test, y.test),
-                         shuffle = FALSE)
+                         shuffle = TRUE)
+## ==================================================================
+## save model
+model %>% save_model_hdf5(filepath=paste0(checkpoint_dir,"/lstm01.h5"))
+## load (optimal) model weights
+model %>% load_model_weights_hdf5(filepath=paste0(checkpoint_dir,"/weights.12-0.13.hdf5"))
+
+## ==================================================================
+## do prediction
 model %>% evaluate(x.test, y.test, batch_size=batch.size)
 pred.test  <- model %>% predict(x.test, y.test, batch_size=batch.size)
 pred.train <- model %>% predict(x.train, y.train, batch_size=batch.size)
 dat.pred <- data.frame(obs=c(y.test,y.train), pred=c(pred.test,pred.train), testtrain=c(rep("test",length(y.test)),rep("train",length(y.train))),
                        rw=1:(length(y.test)+length(y.train))) %>% gather(key="key",value = "value",-rw,-testtrain)
-feet.meter <- 0.3048
-gg.train <- ggplot(dat.pred%>%filter(testtrain=="train")%>%mutate(value=(value*scld$scale+scld$center)*feet.meter^3), aes(x=rw, y=value, color=key)) + geom_point()
+dat.pred <- dat.pred %>%mutate(value=unscale.log(list(y=value,scl=data$scl)))
+
+## ==================================================================
+## plot prediction
+gg <- ggplot() + theme_bw() + labs(y="Discharge (mm/d)", x="Time", color="") + theme(text = element_text(size=16))
+gg.train <- gg + geom_line(aes(x=rw, y=value, color=key), data=dat.pred%>%filter(testtrain=="train")) + labs(caption="calibration")
 plot(gg.train)
-gg.test <- ggplot(dat.pred%>%filter(testtrain=="test")%>%mutate(value=(value*scld$scale+scld$center)*feet.meter^3), aes(x=rw, y=value, color=key)) + geom_line() + theme_bw() + labs(y=expression("Discharge ("*m^3*"/s)"), x="Time", color="") + theme(text = element_text(size=16))
-ggsave("pred_streamflow_validation.png", plot = gg.test, width=8)
+gg.test <- gg + geom_line(aes(x=rw, y=value, color=key), data=dat.pred%>%filter(testtrain=="test")) + labs(caption="validation")
 plot(gg.test)
 
 ## construct LSTM plus catchment properties NN
 batch.size <- 10
-inp.ts <- layer_input(batch_shape = c(batch.size, lags, ncol(forc1)))
+inp.ts <- layer_input(batch_shape = c(batch.size, lags+1, ncol(forc1)))
 mylstm <- inp.ts %>% layer_lstm(units = 100,
                                 batch_size = batch.size,
                                 return_sequences = TRUE,
